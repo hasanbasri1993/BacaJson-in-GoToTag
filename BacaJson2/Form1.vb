4bf7bae8399536641c53a77c41903ed8b3587c71
Hasan Basri <hasanbasri1493@gmail.com>
2022-09-29 21:07:43 +0700

    commit

---
+++ BacaJson2/Form1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim json As String = File.ReadAllText("C:\result.txt")
+        Dim jsonres = JsonConvert.DeserializeObject(json)
+        Dim decodeBase64 = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(jsonres(0)("Text")))
+        Dim jsonFromBase64 = JsonConvert.DeserializeObject(decodeBase64)
+        SendKeys.SendWait("%{TAB}")
+        Threading.Thread.Sleep(300)
+        SendKeys.SendWait(jsonFromBase64("induk"))
+        SendKeys.SendWait("{ENTER}")
+        Application.Exit()
+    End Sub
+End Class
